713bb88294707923b161bf4bb921dc12971106a4
harshcurious <eharshkmr@gmail.com>
2021-10-24 18:17:35 +0530

    bike-share stage1: AssertError

--- project-bikesharing/debugging_version.py
+++ project-bikesharing/debugging_version.py
@@ -67,13 +67,11 @@ class NeuralNetwork(object):
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
         hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
-#         hidden_outputs = self.activation_function(hidden_inputs)
-        hidden_outputs = self.activation_function(hidden_inputs)# signals from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with your calculations.
         final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
-        final_outputs = self.activation_function(final_inputs)
-#         final_outputs = (final_inputs)# signals from final output layer
+        final_outputs = self.activation_function(final_inputs) # signals from final output layer
         
         return final_outputs, hidden_outputs
 
@@ -93,23 +91,21 @@ class NeuralNetwork(object):
 
         # TODO: Output error - Replace this value with your calculations.
         output_error = y - final_outputs # Output layer error is the difference between desired target and actual output.
-        output_error = output_error[:,None]
+        
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-#         output_error_term = output_error * final_outputs
         output_error_term = output_error * final_outputs * (1 - final_outputs)
         
         # TODO: Calculate the hidden layer's contribution to the error
         hidden_error = np.dot(output_error_term, self.weights_hidden_to_output.T)
         
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
-        #print(hidden_error_term.shape)
+        
         # TODO: Add Weight step (input to hidden) and Weight step (hidden to output).
         # Weight step (input to hidden)
         delta_weights_i_h += np.dot(X[:,None], hidden_error_term)
         # Weight step (hidden to output)
-        #print(output_error_term.shape)
-        delta_weights_h_o += np.dot(hidden_outputs[:,None], output_error_term)
+        delta_weights_h_o += np.dot(hidden_outputs.T, output_error_term)
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -150,7 +146,67 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1000
-learning_rate = 0.01
-hidden_nodes = 2000
+iterations = 100
+learning_rate = 0.1
+hidden_nodes = 2
 output_nodes = 1
+
+
+import unittest
+
+inputs = np.array([[0.5, -0.2, 0.1]])
+targets = np.array([[0.4]])
+test_w_i_h = np.array([[0.1, -0.2],
+                       [0.4, 0.5],
+                       [-0.3, 0.2]])
+test_w_h_o = np.array([[0.3],
+                       [-0.1]])
+
+class TestMethods(unittest.TestCase):
+    
+    ##########
+    # Unit tests for data loading
+    ##########
+    
+    def test_data_path(self):
+        # Test that file path to dataset has been unaltered
+        self.assertTrue(data_path.lower() == 'bike-sharing-dataset/hour.csv')
+        
+    def test_data_loaded(self):
+        # Test that data frame loaded
+        self.assertTrue(isinstance(rides, pd.DataFrame))
+    
+    ##########
+    # Unit tests for network functionality
+    ##########
+
+    def test_activation(self):
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        # Test that the activation function is a sigmoid
+        self.assertTrue(np.all(network.activation_function(0.5) == 1/(1+np.exp(-0.5))))
+
+    def test_train(self):
+        # Test that weights are updated correctly on training
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+        
+        network.train(inputs, targets)
+        self.assertTrue(np.allclose(network.weights_hidden_to_output, 
+                                    np.array([[ 0.37275328], 
+                                              [-0.03172939]])))
+        self.assertTrue(np.allclose(network.weights_input_to_hidden,
+                                    np.array([[ 0.10562014, -0.20185996], 
+                                              [0.39775194, 0.50074398], 
+                                              [-0.29887597, 0.19962801]])))
+
+    def test_run(self):
+        # Test correctness of run method
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        self.assertTrue(np.allclose(network.run(inputs), 0.09998924))
+
+suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
+unittest.TextTestRunner().run(suite)
